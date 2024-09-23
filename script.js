968f531dc18e871144a287650907a1a5100ad78c
Deleon <delps.santos1987@gmail.com>
2024-09-23 17:40:30 -0300

    ajustes no corpo do rodape e seleção e colres

--- script.js
+++ script.js
@@ -81,8 +81,23 @@ window.addEventListener('mousemove', (event) => {
     }
 });
 
+const menuHamburger = document.getElementById('menu_hamburger');
+const menuMobile = document.querySelector('.menu_mobile');
+const menuLinks = document.querySelectorAll('.menu_mobile a');
 
+// Adiciona um evento de clique ao botão hamburguer para abrir/fechar o menu
+menuHamburger.addEventListener('click', function() {
+menuHamburger.classList.toggle('active');
+menuMobile.classList.toggle('active');
+});
 
+// Adiciona eventos de clique a cada link do menu mobile para fechar o menu
+menuLinks.forEach(link => {
+link.addEventListener('click', function() {
+    menuHamburger.classList.remove('active'); // Remove a classe 'active' do botão
+    menuMobile.classList.remove('active'); // Remove a classe 'active' do menu mobile
+});
+});
 // Intersection Observer para animações de seção
 const sections = document.querySelectorAll('.corpo, .sobre, .desenvolvimento, .skills');
 const observer = new IntersectionObserver((entries) => {
